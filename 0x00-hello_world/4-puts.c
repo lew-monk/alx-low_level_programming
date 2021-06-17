a65b5772cd9bfb735024db89ae97da05b2e5a150
lewmonk@gmail.com <lewmonk@gmail.com>
2021-06-17 22:53:42 +0300

    Initial Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - is void hence it returns 0 if there is no error
- * It prints to console using the puts function
- * Return: 0 if no error or non zero if there is an error
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
