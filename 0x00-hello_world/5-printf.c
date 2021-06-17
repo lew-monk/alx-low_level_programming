35ea459e80672acb14831ccf3ec4dee6c4692a3a
lewmonk@gmail.com <lewmonk@gmail.com>
2021-06-17 22:42:18 +0300

    Initial Commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints with proper grammar, 
- * but the outcome is a piece of art
+ * but the outcome is a piece of art,
  * Return: 0 if exited properly, otherwise a 1 or any other
  */
 int main(void)
 {   
     printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+    return (0);
 }
