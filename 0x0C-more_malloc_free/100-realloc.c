76b6676f7cf959ee8adc96af82bac493f48882ae
lewmonk@gmail.com <lewmonk@gmail.com>
2021-07-15 20:21:26 +0300

    Advanced Tasks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,51 +1,46 @@
   
-#include <stdlib.h>
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
- * *_realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previsouly allocated by malloc
- * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
- *
- * Return: pointer to the newly allocated memory block
+ * _realloc - a function that reallocates a memory block.
+ * @ptr : void pointer to the allocated memory area .
+ * @old_size : the old size of the allocated memory.
+ * @new_size : the new size of reallocated memory.
+ * Return: void pointer.
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptr1;
-	char *old_ptr;
-	unsigned int i;
+
+/* allocating new dynamic memory without initialization */
+	if (new_size > old_size)
+	{
+		free(ptr);
+			ptr = (int *) malloc(new_size);
+	if (ptr == NULL)
+	return (NULL);
+	else
+	return (ptr);
+	}
+/*the call is equivalent to malloc(new_size)*/
+	if (ptr == NULL)
+	{
+		return (ptr = (int *) malloc(new_size));
+
+			if  (ptr == NULL)
+				return (NULL);
+	}
+/* returns a Void Pointer */
 
 	if (new_size == old_size)
-		return (ptr);
-
+	return (ptr);
+/* freeing Ptr */
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
-		return (NULL);
+			return (NULL);
 	}
 
-	if (!ptr)
-		return (malloc(new_size));
-
-	ptr1 = malloc(new_size);
-	if (!ptr1)
-		return (NULL);
-
-	old_ptr = ptr;
-
-	if (new_size < old_size)
-	{
-		for (i = 0; i < new_size; i++)
-			ptr1[i] = old_ptr[i];
-	}
-
-	if (new_size > old_size)
-	{
-		for (i = 0; i < old_size; i++)
-			ptr1[i] = old_ptr[i];
-	}
-
-	free(ptr);
-	return (ptr1);
+	return (ptr);
 }
