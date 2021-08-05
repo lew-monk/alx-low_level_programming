d7d98321fb2a1e1407f8aa01574bbd497ca00533
lew-monk <lewmonk@gmail.com>
2021-08-05 20:16:31 +0300

    Initial Tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,68 +1,40 @@
-#include <stdio.h>
-/**
- *_strlen -main entry.
- *void: nothing
- *@s: char pointer
- *Description: Function that returns the length of a string.
- * Return: void
- **/
-unsigned int _strlen(const char *s)
-{
-	int c;
 
-	for (c = 0; s[c] != '\0'; c++) /*\0 is the Null character */
-		;
-	return (c);
-}
-/**
- *_power -main entry.
- *void: nothing
- *@base: base
- *@exponent: exponent
- *Description: Function that returns the base elevated to the exponent
- * Return: result of operation
- **/
-int _power(int base, int exponent)
-{
-	int result = 1;
+#include "holberton.h"
 
-	for (; exponent > 0; exponent--)
-		result = result * base;
-
-	return (result);
-}
 /**
- *binary_to_uint -.
- *@b: binary number in char pointer
- *Description: Function receives binary in char and return decimmal
- * Return: unsigned integer
- **/
+ * binary_to_uint - Converters a binary number to an unsigned integer
+ * @b: String representing a binary number
+ * Return: The converted number or 0 if b is NULL or if there are chars
+ * that are not 0 or 1
+ */
 unsigned int binary_to_uint(const char *b)
-{   /*c = counter pw = power  num=save the decimal*/
-	int len, c, pw, num = 0;
-	unsigned int n; /* n=number*/
+{
+	unsigned int len;
+	unsigned int i;
+	unsigned int j;
+	unsigned int sum;
+	unsigned int pow;
+	int base;
 
-	if (b == NULL) /*validate that the string of chars is not null*/
+	base = 2;
+	sum = 0;
+	pow = 1;
+	if (b == NULL)
 		return (0);
-
-	len =  _strlen(b); /*invoke function to capture the length*/
-
-	for (c = 0; b[c] != '\0'; c++, len--) /*go through the string of chars*/
+	for (len = 0; b[len] != '\0'; len++) /*Getting the length of b num*/
+		;
+	if (len == 1 && (b[0] == '0' || b[0] == '1')) /*Checking single nums*/
+		return ((b[0] - 48));
+	for (i = 0; b[i] != '\0'; i++) /*Iterating through the string*/
 	{
-/*The binary num is as char, so n=b[c]-'0' capture it as unsigned int */
-		n = b[c] - '0';
-
-		if ((n == 0) || (n == 1))/*validate it us a binary number*/
-		{
-/*len -1 beacuse from rigth to left it start from zero*/
-/*to calculate bin to decim from rigth to left is: 2^0*bin+2^2*bin+2^3*bin.. */
-			pw = _power(2, (len - 1));
-/*the sum of the powers is the number in decimal*/
-			num = num + (n * pw);
-		}
-		else
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		for (j = len - 1; j > 0; j--) /*Calculating the exponent*/
+			pow = pow * base;
+		sum = sum + (pow * (b[i] - 48)); /*Adding the number*/
+		len--;
+		pow = 1; /*Reseting the exponent*/
 	}
-	return (num);
+	return (sum);
 }
 
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,37 +1,29 @@
-#include <stdio.h>
 #include "holberton.h"
+void print_b(unsigned long int n);
 /**
- *print_binary -.
- *@n: int number
- *Description: Function that receives an int and prints the binary
- * Return: void
- **/
-
+ * print_binary - Prints the binary representation of a number
+ * @n: Number to interpret as binary
+ */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1, copy = n; /*copy of int in binary*/
-	unsigned long int contBits = 0; /*counter of bits*/
-
 	if (n == 0)
-	{
 		_putchar('0');
+	else
+		print_b(n);
+}
+
+/**
+ * print_b - Prints the binary representation of a number
+ * @n: Number to interpret as binary
+ */
+void print_b(unsigned long int n)
+{
+	if (n == 0)
 		return;
-	}
-	while (copy) /*copy != 0*/
-	{
-		copy = copy >> 1;
-		contBits++;
-	}
-	mask = mask << (contBits - 1);
-
-	while (mask) /*mask != 0*/
-	{
-		if (mask & n)
-			_putchar('1');
-		else
-			_putchar('0');
-
-		mask = mask >> 1;
-	}
+	print_b((n >> 1));
+	if ((n & 1) == 1)
+		_putchar('1');
+	if ((n & 1) == 0)
+		_putchar('0');
 }
 
